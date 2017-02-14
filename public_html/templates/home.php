e96cdde856dfd453d0f34ed465accc26c97c7294
Joseph Rohrer <josephrohrer@me.com>
2017-02-13 23:42:56 -0700

    Added home

---
+++ public_html/templates/home.php
@@ -0,0 +1,8 @@
+<section class="welcome">
+	<div class="container">
+		<div class="jumbotron text-center">
+			<h1>Welcome to the Angular Setup!</h1>
+			<p class="lead">This is a bare-bones Angular 2 front end.</p>
+		</div>
+	</div>
+</section>
